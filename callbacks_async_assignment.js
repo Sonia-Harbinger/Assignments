6fd087ea6c8cb3749695bf0ad8f7a537a2311f7d
Sonia Sahu <sonia.sahu@harbingergroup.com>
2025-01-20 22:05:57 +0530

    first commit

---
+++ callbacks_async_assignment.js
@@ -0,0 +1,154 @@
+//TASK 1
+
+
+// Create a function named multiplyByTwo that takes a number as input and multiplies it by 2.
+// Instead of directly returning the result, this function should invoke a callback function with the
+// calculated value as an argument.
+
+function multiplyByTwo(num, callback) {
+let result = num *2; //multiplying number by 2
+callback(result); //invoking callback function
+}
+function usingCallback(result){
+    console.log(result); //displaying result
+}
+
+
+
+
+//TASK 2
+// Use the built-in fs (File System) module to read the contents of a file named "data.txt."
+// Create a function named readFileContents that takes a filename and a callback function as
+// parameters. Inside the function, read the file contents asynchronously using fs.readFile and pass
+// the contents to the callback function.
+
+const fs = require('fs');
+
+function readFileContents(filename, callback) {
+    fs.readFile(filename, 'utf8', (err, data) => {
+        if (err) {
+            return callback(err);
+        }
+        callback(data);
+    });
+}
+
+readFileContents('data.txt',(callback)=>
+{
+    console.log("2nd task called",callback);
+}
+);
+
+
+
+
+//TASK 3
+
+// Create a function named sumArray that takes an array of numbers as input and returns a
+// Promise. The Promise should resolve with the sum of all the numbers in the array.
+// Create a function named squareArray that takes an array of numbers as input and returns a
+// Promise. The Promise should resolve with a new array containing the square of each number in
+// the input array
+function sumArray(arr){
+    return new Promise((resolve, reject)=>{
+        let sum= 0;
+        if(arr.length<=0)
+        {
+            reject("Array does not contain elements");
+        }
+        else{
+            for(let i=0;i<arr.length;i++)
+            {
+                sum = sum + arr[i];
+            }
+            resolve(sum);
+        }
+    })
+};
+function squareArray(arr){
+    return new Promise((resolve, reject)=>{
+        let sqr=[];
+        if(arr.length<= 0)
+        {
+            reject("Array does not contain elements");
+        }
+        else{
+            for(let i=0;i<arr.length;i++)
+            {
+                sqr.push(arr[i]*arr[i]);
+            }
+            resolve(sqr);
+        }
+    }
+    )
+}
+
+//TASK 4
+
+// Refactor the readFileContents function from Task 2 to use async/await. The function should now
+// return the file contents instead of using a callback.
+
+const fs_2 = require('fs');
+
+function readFileContent(filename, callback) {
+    fs_2.readFile(filename, 'utf8', (err, data) => {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, data);
+    });
+}
+// TASK 5
+// Use multiplyByTwo function from Task 1 to multiply a given number by 2 and display the result
+// using console.log.
+// Use the readFileContents function from Task 2 to read the contents of "data.txt" and display the
+// contents using console.log.
+// Use the sumArray function from Task 3 to find the sum of an array of numbers and display the
+// result using console.log.
+// Use the squareArray function from Task 3 to square an array of numbers and display the resulting
+// squared array using console.log.
+// Test the refactored readFileContents function from Task 4 and display the file contents using
+// console.log.
+
+multiplyByTwo(5, usingCallback); //calling function of task 1
+
+
+readFileContents('data.txt',(data)=>{
+    console.log(data);
+}); //calling function of task 2
+
+
+
+
+sumArray([1,2,3,4,5])
+.then(result=>console.log(result))
+.catch(error=>console.log(error));
+
+
+squareArray([1,2,3,4,5])
+.then(result=>console.log(result))
+.catch(error=>console.log(error));
+ //calling function of task 3
+
+
+
+ readFileContent('data.txt', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+    } else {
+        console.log('File contents:', data);
+    }
+}); //calling function of task 4
+
+
+
+//BONUS TASK
+function delay(time) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, time);
+    });
+}
+
+delay(2000).then(() => {
+    console.log('Resolved after 2 seconds');
+});
---
+++ variables_assignment.js
@@ -0,0 +1,60 @@
+//TASK 1
+let name = "Sonia Sahu"; //my name
+let age = 22; //my age
+let isStudent = true; //I am a student hence true
+
+
+
+//TASK 2
+let num1 = 30; //first number
+let num2 = 50; //second number
+let sum = num1 + num2; //sum of two numbers
+let difference = num1 - num2; //difference of two numbers
+let product = num1 * num2; //product of two numbers
+let quotient = num1 / num2; //quotient of two numbers
+
+
+
+
+
+//TASK 3
+
+let firstName = "Sonia"; //my first name
+let lastName = "Sahu"; //my last name
+let occupation = "Trainee Software Engineer"; //my occupation
+let fullName = firstName + " " + lastName; //concatenating first name and last name
+
+let bio =
+  " My name is " +
+  fullName +
+  "." +
+  " I am working as a " +
+  occupation +
+  "at Harbinger Groups."; //concatenating full name and occupation in a sentence.
+
+
+
+
+
+//TASK 4
+console.log("Name:" + name, "Age: " + age, "Are you a student? " + isStudent); //displaying name, age and student status
+console.log("Sum is :" + sum); //displaying sum
+console.log("Difference is: " + difference); //displaying difference
+console.log("Product is : " + product); //displaying product
+console.log("Quotient is : " + quotient); //displaying quotient
+console.log("Full Name:" + fullName); //displaying full name
+console.log("BIO:" + bio); // displaying bio
+
+
+
+
+
+
+//Bonus Task
+// Create a variable named "birthYear" and initialize it with your birth year.
+// Calculate your approximate age using the current year and the "birthYear" variable.
+// Display the calculated age using console.log().
+
+var birthYear = 2001; //my birth year
+let myAge = 2025 - birthYear; //calculating age
+console.log("Age: " + myAge); //displaying age
